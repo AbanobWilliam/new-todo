d457250e645f9fd0c120d250bebb3d79da288d25
Abanob William <abanob.william@dxwand.com>
2023-03-21 12:27:15 +0200

    Add and delete Folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,31 @@
 <script>
 	import { Folders } from '$lib/Stores/FolderStore.ts';
 	let foldersArr;
+	let folderName = '';
+	let disabled = true;
 	Folders.subscribe((folders) => {
 		foldersArr = folders;
 	});
+	let checkValue = (e) => {
+		if (e.target.value.length > 2) {
+			disabled = false;
+			folderName = e.target.value;
+		}
+	};
+	let addFolder = () => {
+		console.log(folderName);
+		Folders.subscribe((folders) => {
+			folders.push({ id: folders.length + 1, name: folderName });
+			foldersArr = folders;
+			folderName = '';
+		});
+	};
+	let delteFolder = (folderId) => {
+		const deltedFolder = foldersArr.filter((object) => {
+			return object.id !== folderId;
+		});
+		foldersArr = deltedFolder;
+	};
 </script>
 
 <div class="container">
@@ -13,8 +35,8 @@
 				<h2>Folders</h2>
 				<div class="folder-form mb-3">
 					<div class="input-group">
-						<input type="text" class="form-control" />
-						<button class="btn btn-primary">Add</button>
+						<input type="text" class="form-control" value={folderName} on:input={checkValue} />
+						<button class="btn btn-primary" {disabled} on:click={addFolder}>Add</button>
 					</div>
 				</div>
 				<div class="list-group">
@@ -22,6 +44,7 @@
 						<a href="/folder/{folder.id}" class="list-group-item list-group-item-action">
 							{folder.name}
 						</a>
+						<button class="btn btn-danger" on:click={delteFolder(folder.id)}>Delete</button>
 					{/each}
 				</div>
 			</div>
